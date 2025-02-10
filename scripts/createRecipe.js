2a025a34ba6ebb56c207f7b36c0a09af98d806fa
Cacanuck <156028686+Cacanuck@users.noreply.github.com>
2025-02-10 16:29:39 -0500

    fixed form issues, added a nav and images for the nav

--- scripts/createRecipe.js
+++ scripts/createRecipe.js
@@ -13,6 +13,7 @@ function createIngredientForm() {
   var sizeInput = document.createElement("input");
   sizeInput.setAttribute("type", "text");
   sizeInput.setAttribute("name", "size");
+  sizeInput.classList.add("Amount", "input");
   form.appendChild(sizeInput);
 
   var measureLabel = document.createElement("label");
@@ -21,6 +22,7 @@ function createIngredientForm() {
   var measureInput = document.createElement("input");
   measureInput.setAttribute("type", "text");
   measureInput.setAttribute("name", "measure");
+  measureInput.classList.add("Measure", "input");
   form.appendChild(measureInput);
 
   var ingredientLabel = document.createElement("label");
@@ -29,6 +31,7 @@ function createIngredientForm() {
   var ingredientInput = document.createElement("input");
   ingredientInput.setAttribute("type", "text");
   ingredientInput.setAttribute("name", "ingredient");
+  ingredientInput.classList.add("Ingredient", "input");
   form.appendChild(ingredientInput);
 
   var lastForm = document.querySelector(".ingredientForm:last-of-type");
@@ -42,7 +45,7 @@ function createIngredientForm() {
 }
 
 function createInstructionsForm() {
-  var section = document.querySelector("section");
+  var div = document.querySelector("div");
   var form = document.createElement("form");
   form.setAttribute("method", "post");
   form.setAttribute("action", "/submit");
@@ -56,13 +59,14 @@ function createInstructionsForm() {
   var stepInput = document.createElement("input");
   stepInput.setAttribute("type", "text");
   stepInput.setAttribute("name", "step");
+  stepInput.classList.add("Step", "input");
   form.appendChild(stepInput);
 
   var lastForm = document.querySelector(".instructionForm:last-of-type");
   if (lastForm) {
     lastForm.insertAdjacentElement("afterend", form);
   } else {
-    section.appendChild(form);
+    div.appendChild(form);
   }
 
   return stepInput;
@@ -88,7 +92,33 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
+function createNavLink(href, imgSrc = null) {
+  var listItem = document.createElement("li");
+  var link = document.createElement("a");
+  link.href = href;
+  if (imgSrc) {
+    var image = document.createElement("img");
+    image.src = "./images/" + imgSrc;
+    image.classList.add("navImage");
+    link.appendChild(image);
+  }
+  listItem.appendChild(link);
+  return listItem;
+}
+
+function populateNavLinks() {
+  var nav = document.querySelector("nav");
+  var ul = document.createElement("ul");
+  ul.appendChild(createNavLink("index.html", "GrumpyLogo.svg"));
+  ul.appendChild(createNavLink("#"));
+  ul.appendChild(createNavLink("recipeDisplay.html"));
+  ul.appendChild(createNavLink("#"));
+  ul.appendChild(createNavLink("#", "ProfileLogo.svg"));
+  nav.appendChild(ul);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   createIngredientForm();
   createInstructionsForm();
+  populateNavLinks();
 });
