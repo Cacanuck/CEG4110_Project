f1767289bc029e0272a8d277512c188aeb545caa
Cacanuck <156028686+Cacanuck@users.noreply.github.com>
2025-02-17 16:16:48 -0500

    converted recipe pages to use the defined nav layout and styling

--- scripts/recipeDisplay.js
+++ scripts/recipeDisplay.js
@@ -1,29 +1,60 @@
-function createNavLink(href, imgSrc = null, navTitle) {
-  var listItem = document.createElement("li");
-  var link = document.createElement("a");
-  link.href = href;
-  link.title = navTitle;
-  if (imgSrc) {
-    var image = document.createElement("img");
-    image.src = "./images/" + imgSrc;
-    image.classList.add("navImage");
-    link.appendChild(image);
-  }
-  listItem.appendChild(link);
-  return listItem;
-}
-
-function populateNavLinks() {
+function createNav() {
+  var navLinks = [
+    { text: "Home", href: "index.html" },
+    { text: "Recipes", href: "recipeDisplay.html" },
+    { text: "Inventory", href: "inventory.html" },
+    { text: "Carts", href: "shoppingCart.html" },
+  ];
   var nav = document.querySelector("nav");
+  nav.classList.add("navbar");
   var ul = document.createElement("ul");
-  ul.appendChild(createNavLink("index.html", "GrumpyLogo.svg", "Home"));
-  ul.appendChild(createNavLink("#", "InventoryLogo.svg", "Inventory"));
-  ul.appendChild(
-    createNavLink("recipeDisplay.html", "RecipeLogo.svg", "Recipes")
-  );
-  ul.appendChild(createNavLink("#", "CartLogo.svg", "SHopping Cart"));
-  ul.appendChild(createNavLink("#", "ProfileLogo.svg", "Profile"));
+  ul.classList.add("nav-links");
+  var brandDiv = document.createElement("div");
+  brandDiv.classList.add("nav-brand");
+
+  var logoImg = document.createElement("img");
+  logoImg.setAttribute("class", "logo");
+  logoImg.setAttribute("src", "./images/GrumpyLogo.svg");
+  logoImg.setAttribute("alt", "GrumpyChef Logo");
+  logoImg.setAttribute("width", "40");
+  logoImg.setAttribute("height", "40");
+  brandDiv.appendChild(logoImg);
+
+  var brandText = document.createElement("span");
+  brandText.textContent = "GrumpyChef";
+  brandDiv.appendChild(brandText);
+
+  navLinks.forEach((element) => {
+    var a = document.createElement("a");
+    a.textContent = element.text;
+    a.href = element.href;
+    if (element.text === "Recipes") {
+      a.classList.add("active");
+    }
+    ul.appendChild(a);
+  });
+
+  var profileDiv = document.createElement("div");
+  profileDiv.classList.add("nav-profile");
+  var profileLink = document.createElement("a");
+  profileLink.href = "profile.html";
+  profileLink.classList.add("profile-link");
+  var profileText = document.createElement("span");
+  profileText.textContent = "Profile";
+  profileLink.appendChild(profileText);
+
+  var profileImg = document.createElement("img");
+  profileImg.setAttribute("class", "profile-icon");
+  profileImg.setAttribute("src", "./images/ProfileLogo.svg");
+  profileImg.setAttribute("alt", "Profile Icon");
+  profileImg.setAttribute("width", "30");
+  profileImg.setAttribute("height", "30");
+  profileLink.appendChild(profileImg);
+  profileDiv.appendChild(profileLink);
+
+  nav.appendChild(brandDiv);
   nav.appendChild(ul);
+  nav.appendChild(profileDiv);
 }
 
 function createRecipeButton() {
@@ -104,7 +135,7 @@ function createDiv() {
 
 document.addEventListener("DOMContentLoaded", function () {
   createDiv();
-  populateNavLinks();
+  createNav();
   createRecipeButton();
   editRecipeButton();
   deleteRecipeButton();
