fa5fc9f5808077e14053089e3e940487a49bdef3
Joseph Sluterbeck <josephsluterbeck@gmail.com>
2025-03-23 10:55:09 -0400

    Added new user model class

--- flaskTestTool/models.py
+++ flaskTestTool/models.py
@@ -1,12 +1,14 @@
 from config import db
 
 class recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    dish = db.Column(db.String(100), unique=False, nullable=False)
-    size = db.Column(db.Integer, unique=False, nullable=False)
-    measure = db.Column(db.Integer, unique=False, nullable=False)
-    ingredient = db.Column(db.String(100), unique=False, nullable=False)
-    instruction = db.Column(db.String(200), unique=False, nullable=False)
+    __tablename__ = 'recipe'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    dish = db.Column(db.String(100), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    measure = db.Column(db.Integer, nullable=False)
+    ingredient = db.Column(db.String(100), nullable=False)
+    instruction = db.Column(db.String(200), nullable=False)
     
     def to_json(self):
         return {
@@ -16,4 +18,22 @@ class recipe(db.Model):
             "measure": self.measure,
             "ingredient": self.ingredient,
             "instructions": self.instruction,
+        }
+    
+class newUser(db.Model):
+    __tablename__ = 'new_user'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    firstName = db.Column(db.String(100), nullable=False)
+    lastName = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "email": self.email,
+            "password": self.password
         }
