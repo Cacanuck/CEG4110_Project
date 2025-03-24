5956e8169c6a6c390091fbdab4f02b83bf816200
Cacanuck <156028686+Cacanuck@users.noreply.github.com>
2025-03-23 20:57:11 -0400

    updated recipe db model to have multiple tables

--- flaskTestTool/models.py
+++ flaskTestTool/models.py
@@ -5,21 +5,47 @@ class recipe(db.Model):
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     dish = db.Column(db.String(100), nullable=False)
-    size = db.Column(db.Integer, nullable=False)
-    measure = db.Column(db.Integer, nullable=False)
-    ingredient = db.Column(db.String(100), nullable=False)
-    instruction = db.Column(db.String(200), nullable=False)
+    ingredients = db.relationship('Ingredient', backref='recipe', lazy=True)
+    instructions = db.relationship('Instruction', backref='recipe', lazy=True)
     
     def to_json(self):
         return {
             "id": self.id,
             "dish": self.dish,
+            "ingredients": [ingredient.to_json() for ingredient in self.ingredients],
+            "instructions": [instruction.to_json() for instruction in self.instructions],
+        }
+    
+class ingredient(db.model):
+    __tablename__ = 'ingredient'
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    measure = db.Column(db.Integer, nullable=False)
+    ingredient = db.Column(db.String(100), nullable=False)
+    
+    def to_json(self):
+        return {
+            "id": self.id,
             "size": self.size,
             "measure": self.measure,
             "ingredient": self.ingredient,
-            "instructions": self.instruction,
         }
+        
+class Instruction(db.Model):
+    __tablename__ = 'instruction'
     
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    instruction = db.Column(db.String(200), nullable=False)
+    
+    def to_json(self):
+        return {
+            "id": self.id,
+            "instruction": self.instruction,
+        }
+
 class newUser(db.Model):
     __tablename__ = 'new_user'
     
